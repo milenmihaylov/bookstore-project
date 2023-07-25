bcc9055fd825d36b6c177e297119ae4c58df967e
Milen Mihaylov <milen.m.mihailov@gmail.com>
2023-07-25 03:25:30 +0300

    PostgreSQL database configuration

--- bookworm/bookworm/settings.py
+++ bookworm/bookworm/settings.py
@@ -68,12 +68,15 @@ WSGI_APPLICATION = 'bookworm.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": env("DATABASE_NAME"),
+        "USER": env("DATABASE_USER"),
+        "PASSWORD": env("PASSWORD"),
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
