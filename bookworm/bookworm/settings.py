c4d4e75d7ecf97ae71e180922c5dfdc9b15cdd81
Milen Mihaylov <milen.m.mihailov@gmail.com>
2023-07-25 21:00:55 +0300

    Added media files configuration

--- bookworm/bookworm/settings.py
+++ bookworm/bookworm/settings.py
@@ -117,6 +117,10 @@ STATICFILES_DIRS = [
 	join(BASE_DIR, 'static'),
 ]
 
+MEDIA_URL = 'media/'
+
+MEDIA_ROOT = join(BASE_DIR, 'media')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
