ceb4fa4b627a6a0654b2a4d83c99155a68271823
Milen Mihaylov <milen.m.mihailov@gmail.com>
2023-07-29 13:32:13 +0300

    Created Article model

--- bookworm/bookworm/blog/models.py
+++ bookworm/bookworm/blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Article(models.Model):
+	writer = models.CharField(
+		max_length=30,
+	)
+	text = models.TextField()
+	image = models.ImageField(
+		upload_to='images/blog',
+	)
