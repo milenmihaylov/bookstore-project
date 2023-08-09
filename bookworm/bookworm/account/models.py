9e88ba063b23bfaa3e5355e18a58c50c6eb3bd43
Milen Mihaylov <milen.m.mihailov@gmail.com>
2023-08-09 21:01:23 +0300

    Created Account model

--- bookworm/bookworm/account/models.py
+++ bookworm/bookworm/account/models.py
@@ -1,3 +1,23 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+
+USER_MODEL = get_user_model()
+
+
+class Account(models.Model):
+	user = models.OneToOneField(
+		to=USER_MODEL,
+		primary_key=True,
+		on_delete=models.CASCADE,
+	)
+
+	first_name = models.CharField(
+		max_length=20,
+		blank=True,
+	)
+
+	last_name = models.CharField(
+		max_length=20,
+		blank=True,
+	)
