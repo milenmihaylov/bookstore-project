b2acd4d1b0ff549d15cc94ce7571a290a53c0ac8
Milen Mihaylov <milen.m.mihailov@gmail.com>
2023-08-09 20:51:40 +0300

    BookwormUser model created

--- bookworm/bookworm/settings.py
+++ bookworm/bookworm/settings.py
@@ -128,3 +128,5 @@ MEDIA_ROOT = join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'bookworm_auth.BookwormUser'
